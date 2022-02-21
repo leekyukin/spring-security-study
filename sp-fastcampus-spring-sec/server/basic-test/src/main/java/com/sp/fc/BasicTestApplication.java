9a43a7915edc51842ec95ca2b735dd1e50d26963
leekyukin <leekujin14@gmail.com>
2022-02-22 00:17:47 +0900

    2022-02-22 Implement simple login method with authemtication

--- sp-fastcampus-spring-sec/server/basic-test/src/main/java/com/sp/fc/BasicTestApplication.java
+++ sp-fastcampus-spring-sec/server/basic-test/src/main/java/com/sp/fc/BasicTestApplication.java
@@ -8,9 +8,6 @@ public class BasicTestApplication {
 
     public static void main(String[] args) {
 
-        Person person = Person.builder().name("test").build();
-        System.out.println(person);
-
         SpringApplication.run(BasicTestApplication.class, args);
     }
 }
