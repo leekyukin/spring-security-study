d1ea1fb79cb34f9df464e365d686213f6ff8513d
leekyukin <leekujin14@gmail.com>
2022-02-23 19:43:38 +0900

    2022-02-23 Implement custom filter

---
+++ login-suctom-filter1/src/main/java/com/kyukin/loginsuctomfilter1/LoginSuctomFilter1Application.java
@@ -0,0 +1,13 @@
+package com.kyukin.loginsuctomfilter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSuctomFilter1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginSuctomFilter1Application.class, args);
+    }
+
+}
