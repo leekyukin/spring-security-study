d72dd03fcc4e97801f0b113694504bb4da808560
leekyukin <leekujin14@gmail.com>
2022-02-24 01:30:46 +0900

    2022-02-24 spring security tutorial with BasicAuthentication implementation is complete

---
+++ basic-authentication/src/main/java/com/kyukin/basicauthentication/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.kyukin.basicauthentication.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "hello";
+    }
+
+    @PostMapping("/greeting")
+    public String greeting(@RequestBody String name) {
+        return "hello " + name;
+    }
+}
