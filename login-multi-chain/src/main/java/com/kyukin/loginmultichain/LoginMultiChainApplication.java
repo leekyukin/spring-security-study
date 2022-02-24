b00d8d7464ac5310970f1ed4d206ef5bc9d551dd
leekyukin <leekujin14@gmail.com>
2022-02-24 12:07:18 +0900

    2022-02-24 Before setting Mobile version

---
+++ login-multi-chain/src/main/java/com/kyukin/loginmultichain/LoginMultiChainApplication.java
@@ -0,0 +1,13 @@
+package com.kyukin.loginmultichain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginMultiChainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginMultiChainApplication.class, args);
+    }
+
+}
---
+++ login-multi-chain/src/test/java/com/kyukin/loginmultichain/LoginMultiChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyukin.loginmultichain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginMultiChainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
