c81ed9ff2645c1ca953cf1a5be5ab7f15bf85e54
leekyukin <leekujin14@gmail.com>
2022-02-25 21:16:32 +0900

    2022-02-25 Implement service

---
+++ user-admin/src/main/java/com/security/useradmin/service/SpUserService.java
@@ -0,0 +1,26 @@
+package com.security.useradmin.service;
+
+import com.security.useradmin.repository.SpUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class SpUserService implements UserDetailsService {
+
+    private final SpUserRepository spUserRepository;
+
+    public SpUserService(SpUserRepository spUserRepository) {
+        this.spUserRepository = spUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return spUserRepository.findSpUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+}
