518b04d410f373c700f6676aeed55619c2fd681d
leekyukin <leekujin14@gmail.com>
2022-05-21 21:08:23 +0900

    ADD :: Logout test

--- inflearn-security/src/main/java/com/study/inflearnsecurity/SecurityConfig.java
+++ inflearn-security/src/main/java/com/study/inflearnsecurity/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -32,13 +35,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .usernameParameter("userid")    // 아이디 파라미터명 설정
                 .passwordParameter("passwd")    // 비밀번호 필드명 설정
                 .loginProcessingUrl("/login_proc")   // from 태그의 action url 변경 | default : login
-                .successHandler(new AuthenticationSuccessHandler() {
+                .successHandler(new AuthenticationSuccessHandler() {    // 로그인 성공시 함수 호출
                     @Override
                     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
                         System.out.println("authentication : " + authentication.getName());    // 인증된 객체의 이름 보여주기
                         response.sendRedirect("/");     // redirect : url root
                     }
-                })   // 로그인 성공시 함수 호출
+                })
                 .failureHandler(new AuthenticationFailureHandler() {
                     @Override
                     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
@@ -48,5 +51,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 })
                 .permitAll()    // loginPage 의 접근을 인증없이 허용함
         ;
+
+        http
+                .logout()
+                .logoutUrl("/logout")   // default : /logout
+                .logoutSuccessUrl("/login") // login 성공 후 이동 페이지
+                .addLogoutHandler(new LogoutHandler() { // 로그아웃 핸들러 설정
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();   // 세션 무효화
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {  // 로그아웃 성공 후 핸들러 호출
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");
+                    }
+                })
+                .deleteCookies("remember-me")
+                ;
     }
 }
