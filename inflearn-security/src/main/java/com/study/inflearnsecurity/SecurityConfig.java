80443c7e5dbe933758632006dffe84c1738b6a30
leekyukin <leekujin14@gmail.com>
2022-05-25 16:08:31 +0900

    ADD :: 권한설정하기

--- inflearn-security/src/main/java/com/study/inflearnsecurity/SecurityConfig.java
+++ inflearn-security/src/main/java/com/study/inflearnsecurity/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.study.inflearnsecurity;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,24 +12,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+                                                                // 비밀번호 암호화 정책을 prefix 로 적어줘야함
+                                                                    //    [noop] -> none
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1234").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1234").roles("SYS", "USER");
+        auth.inMemoryAuthentication() .withUser("admin").password("{noop}1234").roles("ADMIN","SYS","USER");
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
                 .authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 .anyRequest().authenticated()
         ;
         http
                 .formLogin()
         ;
-        http
-                .sessionManagement()
-                .sessionFixation().none() // 기본값 // 접속할때마다 새로운 세션 발급, 서블릿 3.1 이상에서 기본값
-                                                     // none : 새로 생성하지 않음, 공격당함
-                                                     // migrateSession : 서블릿 3.1 이하에서 기본값, changeSessionId()와 같음
-                                                     // newSession : JSESSIONID 가 새로 발급되지만 이전 세션에서 설정한 속성값은 초기화 된다.
-        ;
-
     }
 }
