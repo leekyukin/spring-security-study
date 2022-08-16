983d733df5e41fe056398446e24a692a9dc58c46
leekyukin <leekujin14@gmail.com>
2022-08-16 11:59:53 +0900

    ADD :: User domain

---
+++ jwt-study/src/main/java/com/study/jwtstudy/domain/user/domain/Role.java
@@ -0,0 +1,5 @@
+package com.study.jwtstudy.domain.user.domain;
+
+public enum Role {
+    ROLE_USER, ROLE_ADMIN;
+}
---
+++ jwt-study/src/main/java/com/study/jwtstudy/domain/user/domain/User.java
@@ -0,0 +1,29 @@
+package com.study.jwtstudy.domain.user.domain;
+
+import com.study.jwtstudy.domain.user.domain.Role;
+import com.study.jwtstudy.global.entity.BaseTimeEntity;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "users")
+public class User extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Role role;
+}
