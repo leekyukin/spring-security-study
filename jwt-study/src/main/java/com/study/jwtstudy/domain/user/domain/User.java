28878c99fba8f70567b494089c929d3f47e493ce
leekyukin <leekujin14@gmail.com>
2022-08-17 23:30:59 +0900

    ADD :: User Domain

--- jwt-study/src/main/java/com/study/jwtstudy/domain/user/domain/User.java
+++ jwt-study/src/main/java/com/study/jwtstudy/domain/user/domain/User.java
@@ -2,12 +2,17 @@ package com.study.jwtstudy.domain.user.domain;
 
 import com.study.jwtstudy.domain.user.domain.type.Role;
 import com.study.jwtstudy.global.entity.BaseTimeEntity;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Getter
+@NoArgsConstructor
 @Table(name = "users")
 public class User extends BaseTimeEntity {
 
@@ -24,6 +29,21 @@ public class User extends BaseTimeEntity {
     @Column(nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
+
+    @Builder
+    public User (String name, String email, String password, Role role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public void matchedPassword(PasswordEncoder passwordEncoder, String password) {
+        if (!passwordEncoder.matches(password, this.password)) {
+            throw new RuntimeException("비밀번호 틀림");
+        }
+    }
 }
--- jwt-study/src/main/java/com/study/jwtstudy/domain/user/domain/repository/UserRepository.java
+++ jwt-study/src/main/java/com/study/jwtstudy/domain/user/domain/repository/UserRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> finDByEmail(String username);
+    Optional<User> findByEmail(String username);
+    boolean existsByEmail(String email);
 }
